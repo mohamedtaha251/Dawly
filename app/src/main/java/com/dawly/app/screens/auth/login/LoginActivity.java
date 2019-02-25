a96b924723269778dab2d22a79783660d9207787
mohamed.kamal <me.muhammad.kamal@gmail.com>
2019-02-25 16:06:17 +0200

    Code Refactor

--- app/src/main/java/com/dawly/app/screens/auth/login/LoginActivity.java
+++ app/src/main/java/com/dawly/app/screens/auth/login/LoginActivity.java
@@ -3,28 +3,38 @@ package com.dawly.app.screens.auth.login;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.PatternMatcher;
+import android.support.design.widget.TextInputLayout;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 import app.dawly.com.dawly.R;
 import app.dawly.com.dawly.databinding.ActivityLoginBinding;
 import com.dawly.app.base.BaseActivity;
+import com.dawly.app.base.BaseContract;
 import com.dawly.app.entities.response.LoginResponse;
 import com.dawly.app.entities.User;
 import com.dawly.app.entities.response.SignUpResponse;
 import com.dawly.app.utils.Validation;
+import com.dawly.app.views.DawlyEditText;
 import com.facebook.CallbackManager;
 import dagger.android.AndroidInjection;
 
 import javax.inject.Inject;
-import java.util.regex.Pattern;
 
-public class LoginActivity extends BaseActivity implements LoginContract.LoginInteractor {
+public class LoginActivity extends BaseActivity implements LoginContract.LoginInteractor, BaseContract.ClickListener {
     @Inject
     LoginPresenterImpl loginPresenter;
     CallbackManager callbackManager;
     ActivityLoginBinding loginBinding;
+    DawlyEditText etSignUpFirstName,
+            etSignUpLastName,
+            etSignUpPassword,
+            etSignUpConfirmPassword,
+            etSignUpEmail,
+            etPhoneNumber;
+
+    TextInputLayout signUpEmailInput, signUpPasswordInput, confirmPasswordInput, firstNameInput, lastNameInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,77 +43,25 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginIn
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
-        //    loginBinding.setClickListener(this);
+        etSignUpFirstName = loginBinding.signUpFrame.signupFirstName;
+        etSignUpLastName = loginBinding.signUpFrame.etSignUpLastName;
+        etSignUpPassword = loginBinding.signUpFrame.signupPassword;
+        etPhoneNumber = loginBinding.signUpFrame.etPhoneNumber;
+        etSignUpConfirmPassword = loginBinding.signUpFrame.signupConfirmPassword;
+        etSignUpEmail = loginBinding.signUpFrame.signupEmail;
+        signUpEmailInput = loginBinding.signUpFrame.signupEmailInput;
+        signUpPasswordInput = loginBinding.signUpFrame.signupPasswordInput;
+        confirmPasswordInput = loginBinding.signUpFrame.signupConfirmPasswordInput;
+        firstNameInput = loginBinding.signUpFrame.signupFirstNameInput;
+        lastNameInput = loginBinding.signUpFrame.lastNameInput;
+
+
+        loginBinding.setClickListener(this);
         loginBinding.signCard.bringToFront();
         getSupportActionBar().hide();
 
-        actions();
-
     }
 
-    private void actions() {
-        loginBinding.btnSwitchToSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginBinding.signInFrame.setVisibility(View.VISIBLE);
-                loginBinding.signUpFrame.setVisibility(View.INVISIBLE);
-                loginBinding.loginBtn.setText(R.string.sign_in);
-
-            }
-        });
-
-        loginBinding.btnSwitchToSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginBinding.signInFrame.setVisibility(View.INVISIBLE);
-                loginBinding.signUpFrame.setVisibility(View.VISIBLE);
-                loginBinding.loginBtn.setText(R.string.sign_up);
-            }
-        });
-
-        loginBinding.loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (loginBinding.signInFrame.getVisibility() == View.VISIBLE) {
-                    if (validateLogin()) {
-                        //collect data from UI
-                        String email = loginBinding.loginEmail.getText().toString();
-                        String password = loginBinding.loginPassword.getText().toString();
-
-                        //create user
-                        User user = new User();
-                        user.setEmail(email);
-                        user.setPassword(password);
-
-                        loginPresenter.loginWithFacebook(user);
-                    }
-                }
-
-                if (loginBinding.signUpFrame.getVisibility() == View.VISIBLE) {
-                    if (validateSignup()) {
-                        //collect data from UI
-                        String fname = loginBinding.signupFirstName.getText().toString();
-                        String lname = loginBinding.etSignUpLastName.getText().toString();
-                        String password = loginBinding.signupPassword.getText().toString();
-                        String email = loginBinding.signupEmail.getText().toString();
-                        String phone = loginBinding.etPhoneNumber.getText().toString();
-
-                        //create user
-                        User user = new User();
-                        user.setFisrtName(fname);
-                        user.setLastName(lname);
-                        user.setPassword(password);
-                        user.setEmail(email);
-                        user.setPhone(phone);
-
-                        loginPresenter.signUp(user);
-                    }
-
-
-                }
-            }
-        });
-    }
 
     private boolean validateLogin() {
         if (loginBinding.loginEmail.getText().toString().equals("")) {
@@ -125,68 +83,68 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginIn
     }
 
     private boolean validateSignup() {
-        if (loginBinding.signupFirstName.getText().toString().equals("")) {
-            loginBinding.signupFirstName.setError("First name is required");
+        if (etSignUpFirstName.getText().toString().equals("")) {
+            etSignUpFirstName.setError("First name is required");
             return false;
         }
 
-        if (loginBinding.signupFirstName.getText().toString().length() < 3) {
-            loginBinding.signupFirstName.setError("First name is too short");
+        if (etSignUpFirstName.getText().toString().length() < 3) {
+            etSignUpFirstName.setError("First name is too short");
             return false;
         }
 
-        if (loginBinding.etSignUpLastName.getText().toString().equals("")) {
-            loginBinding.etSignUpLastName.setError("Last name is required");
+        if (etSignUpLastName.getText().toString().equals("")) {
+            etSignUpLastName.setError("Last name is required");
             return false;
         }
 
-        if (loginBinding.etSignUpLastName.getText().toString().length() < 3) {
-            loginBinding.etSignUpLastName.setError("Last name is too short");
+        if (etSignUpLastName.getText().toString().length() < 3) {
+            etSignUpLastName.setError("Last name is too short");
             return false;
         }
 
-        if (loginBinding.signupPassword.getText().toString().equals("")) {
-            loginBinding.signupPassword.setError("password is required");
+        if (etSignUpPassword.getText().toString().equals("")) {
+            etSignUpPassword.setError("password is required");
             return false;
         }
 
-        if (loginBinding.signupPassword.getText().toString().length() < 6) {
-            loginBinding.signupPassword.setError("at least 6 digit");
+        if (etSignUpPassword.getText().toString().length() < 6) {
+            etSignUpPassword.setError("at least 6 digit");
             return false;
         }
 
-        if (!Validation.isContainLowerCase(loginBinding.signupPassword.getText().toString())) {
-            loginBinding.signupPassword.setError("at least one lower case letter");
+        if (!Validation.isContainLowerCase(etSignUpPassword.getText().toString())) {
+            etSignUpPassword.setError("at least one lower case letter");
             return false;
         }
 
-        if (!Validation.isContainUpperCase(loginBinding.signupPassword.getText().toString())) {
-            loginBinding.signupPassword.setError("at least one upper case letter");
+        if (!Validation.isContainUpperCase(etSignUpPassword.getText().toString())) {
+            etSignUpPassword.setError("at least one upper case letter");
             return false;
         }
 
-        if (!loginBinding.signupPassword.getText().toString().equals(loginBinding.signupConfirmPassword.getText().toString())) {
-            loginBinding.signupConfirmPassword.setError("Password is not matched");
+        if (!etSignUpPassword.getText().toString().equals(etSignUpConfirmPassword.getText().toString())) {
+            etSignUpConfirmPassword.setError("Password is not matched");
             return false;
         }
 
-        if (loginBinding.signupEmail.getText().toString().equals("")) {
-            loginBinding.signupEmail.setError("email is required");
+        if (etSignUpEmail.getText().toString().equals("")) {
+            etSignUpEmail.setError("email is required");
             return false;
         }
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(loginBinding.signupEmail.getText().toString()).matches()) {
-            loginBinding.signupEmail.setError("invalid email");
+        if (!Patterns.EMAIL_ADDRESS.matcher(etSignUpEmail.getText().toString()).matches()) {
+            etSignUpEmail.setError("invalid email");
             return false;
         }
 
-        if (loginBinding.etPhoneNumber.getText().toString().equals("")) {
-            loginBinding.etPhoneNumber.setError("phone is required");
+        if (etPhoneNumber.getText().toString().equals("")) {
+            etPhoneNumber.setError("phone is required");
             return false;
         }
 
-        if (loginBinding.etPhoneNumber.getText().toString().length() < 10) {
-            loginBinding.etPhoneNumber.setError("invalid phone");
+        if (etPhoneNumber.getText().toString().length() < 10) {
+            etPhoneNumber.setError("invalid phone");
             return false;
         }
         return true;
@@ -224,6 +182,62 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginIn
     public void loginSuccess(LoginResponse loginResponse) {
         Toast.makeText(this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.loginBtn:
+                signUp();
+                break;
+            case R.id.btn_switch_to_sign_up:
+                loginBinding.signInFrame.setVisibility(View.INVISIBLE);
+                loginBinding.signUpFrame.signUpLayout.setVisibility(View.VISIBLE);
+                loginBinding.loginBtn.setText(R.string.sign_up);
+                break;
+            case R.id.btn_switch_to_sign_in:
+                loginBinding.signInFrame.setVisibility(View.VISIBLE);
+                loginBinding.signUpFrame.signUpLayout.setVisibility(View.INVISIBLE);
+                loginBinding.loginBtn.setText(R.string.sign_in);
+                break;
+        }
+    }
+
+
+    private void signUp() {
+
+        if (loginBinding.signInFrame.getVisibility() == View.VISIBLE) {
+            if (validateLogin()) {
+                //collect data from UI
+                String email = loginBinding.loginEmail.getText().toString();
+                String password = loginBinding.loginPassword.getText().toString();
+                //create user
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(password);
+                loginPresenter.loginWithFacebook(user);
+            }
+        }
+
+        if (loginBinding.signUpFrame.signUpLayout.getVisibility() == View.VISIBLE) {
+            if (validateSignup()) {
+                //collect data from UI
+                String fname = etSignUpFirstName.getText().toString();
+                String lname = etSignUpLastName.getText().toString();
+                String password = etSignUpPassword.getText().toString();
+                String email = etSignUpEmail.getText().toString();
+                String phone = etPhoneNumber.getText().toString();
+
+                //create user
+                User user = new User();
+                user.setFisrtName(fname);
+                user.setLastName(lname);
+                user.setPassword(password);
+                user.setEmail(email);
+                user.setPhone(phone);
+                loginPresenter.signUp(user);
+            }
+        }
     }
 
 }
