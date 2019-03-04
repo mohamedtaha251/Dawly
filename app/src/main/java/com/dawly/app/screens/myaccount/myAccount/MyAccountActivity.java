2e9e89fd4ccf930ce5d8be3610270c3c6c779494
Mohamed Taha <m.ahmed.taha251@gmail.com>
2019-03-04 23:45:23 +0200

    create myAccount activity and host fragments on it using Navigation Architecture Components

---
+++ app/src/main/java/com/dawly/app/screens/myaccount/myAccount/MyAccountActivity.java
@@ -0,0 +1,14 @@
+package com.dawly.app.screens.myaccount.myAccount;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import app.dawly.com.dawly.R;
+
+public class MyAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
