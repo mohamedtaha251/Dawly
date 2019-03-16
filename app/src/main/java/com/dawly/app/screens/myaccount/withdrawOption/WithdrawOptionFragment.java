f48cccaeb448a77b5efe877d25650196c27dc58d
Mohamed Taha <m.ahmed.taha251@gmail.com>
2019-03-16 22:23:48 +0200

    complete with draw options screen

--- app/src/main/java/com/dawly/app/screens/myaccount/withdrawOption/WithdrawOptionFragment.java
+++ app/src/main/java/com/dawly/app/screens/myaccount/withdrawOption/WithdrawOptionFragment.java
@@ -12,12 +12,15 @@ import app.dawly.com.dawly.R;
 public class WithdrawOptionFragment extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_withdraw_option, container, false);
     }
 
+
+   
 }
